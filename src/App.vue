b5ddf4166de785e7260fde74e387a61e7a3db0ce
Alexandr Smirnov <alexrus37reg@gmail.com>
2025-10-27 20:54:04 +0300

    feat: change env, placeholder

--- src/App.vue
+++ src/App.vue
@@ -80,18 +80,18 @@ const createMobileAnimations = () => {
 
 
 onMounted(async () => {
-  try {
-    const sheetId = '1CDaIrPsdO4MWI7h80yWNcow9Od0U_SDZGJvd2Z9Vigo' // твой ID
-    const url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkiSxlkiKCITuSRhwufp436BFpHg3BF1A2pZobmSMOvrAUQ-FRPVsXOe-iLDq60A/pub?gid=48642444&single=true&output=csv`
-    const { data } = await axios.get(url)
+  // try {
+  //   const sheetId = '1CDaIrPsdO4MWI7h80yWNcow9Od0U_SDZGJvd2Z9Vigo' // твой ID
+  //   const url = `https://docs.google.com/spreadsheets/d/e/2PACX-1vSkiSxlkiKCITuSRhwufp436BFpHg3BF1A2pZobmSMOvrAUQ-FRPVsXOe-iLDq60A/pub?gid=48642444&single=true&output=csv`
+  //   const { data } = await axios.get(url)
 
-    // CSV → массив
-    rows.value = data
-      .split('\n')
-      .map((row: string) => row.split(','))
-  } catch (e) {
-    console.error('Ошибка при загрузке таблицы', e)
-  }
+  //   // CSV → массив
+  //   rows.value = data
+  //     .split('\n')
+  //     .map((row: string) => row.split(','))
+  // } catch (e) {
+  //   console.error('Ошибка при загрузке таблицы', e)
+  // }
   await nextTick();
 
   watch(
